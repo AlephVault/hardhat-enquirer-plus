2dda8bb970102cdb71f471ab50829023d06e8d86
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-05 08:39:11 -0500

    Separating the contracts file.

---
+++ src/common/contracts.js
@@ -0,0 +1,38 @@
+const Enquirer_ = require("enquirer-plus");
+const pathsUtils = require("./paths");
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * Collects all the contract names from the compiled artifacts.
+ * @param hre The hardhat runtime environment.
+ * @returns {*[]} The list of contract names.
+ */
+function collectContractNames(hre) {
+    let contractNames = [];
+    let contractArtifactsDirectoryName = path.resolve(hre.config.paths.artifacts, "contracts");
+    let startPos = contractArtifactsDirectoryName.length + 1;
+    let stat = fs.statSync(contractArtifactsDirectoryName);
+    if (stat.isDirectory()) {
+        pathsUtils.traverseDirectory(contractArtifactsDirectoryName, (subPath, filename) => {
+            if (filename.endsWith('.json') && !filename.endsWith('.dbg.json')) {
+                const contractName = path.basename(filename, '.json');
+                contractNames.push({
+                    name: contractName,
+                    path: subPath.substring(startPos)
+                });
+            }
+        });
+    }
+
+    return contractNames;
+}
+
+/**
+ * A Select of the current in-project contract artifacts.
+ */
+class GivenOrContractSelect extends Enquirer_.GivenOrSelect {
+    constructor(options, hre) {
+        super({...options, choices: collectContractNames(hre)});
+    }
+}
