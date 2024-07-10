db18d072e2088ccacf299fcfd94431a97020f4a4
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-09 22:13:22 -0500

    Ensuring there's only one Address Input, based on hre.common.

--- src/common/addresses.js
+++ src/common/addresses.js
@@ -6,20 +6,40 @@ const Enquirer_ = require("enquirer-plus");
  * is given as an index) is given.
  */
 class GivenOrValidAddressInput extends Enquirer_.GivenOrValidInput {
-    constructor({allowAccountIndex, ...options}, validateChecksumAddress, validateAccount, convertAccount) {
+    constructor({hre, allowAccountIndex, ...options}) {
         super({
             ...options, validate: async (v) => {
                 v = typeof v === "string" ? v.trim() : (v || "");
-                if (/^0x[a-fA-F0-9]{40}$/.test(v) && validateChecksumAddress(v)) {
+                if (/^0x[a-fA-F0-9]{40}$/.test(v) && hre.common.isAddress(v)) {
                     return true;
                 }
-                return !!(allowAccountIndex && await validateAccount(v));
+                return !!(allowAccountIndex && await this._validateAccount(v));
             },
             makeInvalidInputMessage: (v) => `Invalid account index or address: ${v}`,
             onInvalidGiven: (v) => console.error(`Invalid given account index or address: ${v}`)
         });
-        this._convertAccount = convertAccount;
+        this._hre = hre;
         this._allowAccountIndex = allowAccountIndex;
+    }
+
+    async _validateAccount(v) {
+        if (typeof v === "string") {
+            if (!/^\d+$/.test(v)) return false;
+            v = parseInt(v);
+            return v >= 0 && v < (await this._hre.common.getSigners()).length;
+        } else {
+            try {
+                const signer = await this._hre.common.getSigner(parseInt(v));
+                return !!(this._hre.common.getAddress(signer));
+            } catch {
+                return false;
+            }
+        }
+    }
+
+    async _convertAccount(v) {
+        const signer = await this._hre.common.getSigner(parseInt(v));
+        return this._hre.common.getAddress(signer);
     }
 
     async result(v) {
