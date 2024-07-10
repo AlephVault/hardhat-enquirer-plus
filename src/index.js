f8dfe7e66dacc745bc7416d44a492d046a92d0a1
Luis Masuelli <luisfmasuelli@gmail.com>
2024-07-10 08:12:44 -0500

    Moving the common things to the top level and finishing unification.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const {extendEnvironment} = require("hardhat/config");
-const {Enquirer, utils} = require("./core");
+const {Enquirer, utils, promptClasses} = require("./core");
+const fixedpoint = require("./fixedpoint");
+const {collectContractNames, GivenOrContractSelect} = require("./contracts");
+const {GivenOrSolidityVersionSelect} = require("./solidity");
+const {GivenOrValidTokenAmountInput, tokenAmounts} = require("./tokens");
+const {GivenOrValidAddressInput} = require("./addresses");
+const {GivenOrValidAccountInput} = require("./accounts");
+const {GivenOrDeployedContractSelect} = require("./deployments");
 
 extendEnvironment((hre) => {
-    const commonExtender = require("./common");
-    commonExtender(hre);
+    utils.fixedpoint = fixedpoint;
+    utils.contractNames = collectContractNames;
+    utils.tokenAmounts = tokenAmounts;
+    // TODO: Actually, create one subclass for each of these, dynamically providing
+    //       the hre instance among the options, and get rid of the setHRE calls.
+    Enquirer.GivenOrSolidityVersionSelect = GivenOrSolidityVersionSelect;
+    Enquirer.GivenOrContractSelect = GivenOrContractSelect;
+    Enquirer.GivenOrValidTokenAmountInput = GivenOrValidTokenAmountInput;
+    Enquirer.GivenOrValidAddressInput = GivenOrValidAddressInput;
+    Enquirer.GivenOrValidAccountInput = GivenOrValidAccountInput;
+    Enquirer.GivenOrDeployedContractSelect = GivenOrDeployedContractSelect;
+    promptClasses["plus:hardhat:given-or-contract-select"] = GivenOrContractSelect;
+    promptClasses["plus:hardhat:given-or-token-amount-input"] = GivenOrValidTokenAmountInput;
+    promptClasses["plus:hardhat:given-or-solidity-version-select"] = GivenOrSolidityVersionSelect;
+    promptClasses["plus:hardhat:given-or-valid-address-input"] = GivenOrValidAddressInput;
+    promptClasses["plus:hardhat:given-or-valid-account-input"] = GivenOrValidAccountInput;
+    promptClasses["plus:hardhat:given-or-deployed-contract-select"] = GivenOrDeployedContractSelect;
     hre.enquirerPlus = {
         Enquirer, utils
     };
